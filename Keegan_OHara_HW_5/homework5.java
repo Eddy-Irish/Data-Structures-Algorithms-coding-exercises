835bc325a97f35f7ddac502c7c8cd15cab622334
kohara2794 <keeganohara7@gmail.com>
2015-10-12 21:01:36 -0400

    HW 5 files

--- Keegan_OHara_HW_5/homework5.java
+++ Keegan_OHara_HW_5/homework5.java
@@ -1,16 +1,49 @@
+/** Keegan O'Hara
+    CS124
+    Homework 5 main program file */
+
 public class homework5  {   
    
    public static void main(String[] args) {
 
-      BinarySearchTree tree = new BinarySearchTree();
+      // Create the trees
+      BinarySearchTree bstTree = new BinarySearchTree();
+      AvlTree avlTree = new AvlTree();
+      SplayTree splayTree = new SplayTree();
       
-      for (int i = 0; i < 11; i++) {
-         tree.insert(i);
+      
+      
+      // Timing of insertion of 100,000 nodes of increasing value into BST
+      long begin = System.nanoTime();
+      for (int i = 0; i <= 100000; i++)   {
+         bstTree.insert(i);
       }
-      tree.printTree();
-      tree.insert(-1);
-      System.out.println("\n-----------\n");
-      tree.printTree();
+      long end = System.nanoTime();
+      long elapsed = (end - begin);
+      System.out.println("\nBST insertion, increasing : " + elapsed + " nanoseconds.");
+      
+      
+      // Timing of insertion of 100,000 nodes of increasing value into a AVL Tree
+      begin = System.nanoTime();
+      for (int j = 0; j <= 100000; j++) {
+         avlTree.insert(j);
+      }
+      end = System.nanoTime();
+      elapsed = (end - begin);
+      System.out.println("\nAVL insertion, increasing : " + elapsed + " nanoseconds.");
+      
+      
+      // Timing of insertion of 100,000 nodes of increasing value into a Splay Tree
+      begin = System.nanoTime();
+      for (int k = 0; k <= 100000; k++) {
+         splayTree.insert(k);
+      }
+      end = System.nanoTime();
+      elapsed = (end - begin);
+      System.out.println("\nSplay insertion, increasing : " + elapsed + " nanoseconds.");
+
+      
+      
 
    }
 }
